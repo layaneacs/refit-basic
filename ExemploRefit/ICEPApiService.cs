b5e5333ba8e363724e3958dd26bd2b00e035dc6e
layaneacs <46456257+layaneacs@users.noreply.github.com>
2021-05-24 23:20:56 -0300

    Add files via upload

---
+++ ExemploRefit/ICEPApiService.cs
@@ -0,0 +1,17 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploRefit
+{
+    public interface ICEPApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task <CEPResponse> GetInfoCepAsync(string cep);
+    }
+}
+
+
+//--REF: https://viacep.com.br/
